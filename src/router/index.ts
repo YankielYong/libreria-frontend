b3e541634149414645d52b8c108fbab97b2661db
PatriBarrios <lpatri1706@gmail.com>
2024-05-24 13:04:18 -0400

    UserView

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import AuthSignInView from '@/views/AuthSignInView.vue';
 import AuthSignUpView from '@/views/AuthSignUpView.vue';
-import AuthorTableView from '@/views/AuthorTableView.vue';
 import useAuthStore from '@/store/auth';
+import AuthorTableView from '@/views/AuthorTableView.vue';
+import UserTableView from '@/views/UserTableView.vue';
+import { RoleType } from '@/util/enum/RoleType';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -40,15 +42,36 @@ const router = createRouter({
         requireAuth: false,
       },
     },
+    {
+      path: '/user',
+      name: 'userTable',
+      component: UserTableView,
+      meta: {
+        requireAuth: true,
+        restringed: true,
+        admin: true,
+        librarian: false,
+      },
+    },
   ],
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const store = useAuthStore();
   const isAuth = store.token;
 
-  if (to.meta.requireAuth && isAuth === null) next({name: 'signin'});
-  else next();
+  if(!to.meta.requireAuth) next();
+  else if(isAuth !== null) {
+    if(to.meta.restringed){
+      await store.refresh();
+      const role = store.role;
+      if((role === 'Admin' && to.meta.admin) || (role === 'Librarian' && to.meta.librarian)) 
+        next();
+      else throw new Error('Unhautorized');
+    }
+    else next();
+  }
+  else next({name: 'signin'});
 });
 
 export default router;
---
+++ src/views/UserTableView.vue
@@ -0,0 +1,11 @@
+<template>
+    <UserTable />
+</template>
+
+<script lang="ts" setup>
+import UserTable from '@/components/UserTable.vue';
+</script>
+
+<style scoped>
+
+</style>
