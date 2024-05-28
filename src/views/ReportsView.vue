2b706a87cab61ec3962abeb76519d7bab6abaec8
veroRigual <veronicarigual@yahoo.com>
2024-05-28 16:43:06 -0500

    Reportes terminados uwu

---
+++ src/views/ReportsView.vue
@@ -0,0 +1,17 @@
+<template>
+    <BookByAuthor />
+    <BookBySubject />
+    <UserLoans />
+
+</template>
+
+<script lang="ts" setup>
+
+import BookByAuthor from '@/components/BookByAuthor.vue';
+import BookBySubject from '@/components/BookBySubject.vue';
+import UserLoans from '@/components/UserLoans.vue';
+</script>
+
+<style scoped>
+
+</style>
